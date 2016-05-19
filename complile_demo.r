2025dfea91d4a70925aa90b62dd12a1d50eeb6f2
aj2duncan <duncanaj2@gmail.com>
2016-05-19 09:52:51 +0100

    Updates to presentation. Tweaks to demo documents.

--- complile_demo.r
+++ complile_demo.r
@@ -1,66 +1,41 @@
 #' ---
 #' title: "Example of Compiled Notebook"
 #' author: "Andrew Duncan"
+#' date: ""
 #' output: html_document
 #' ---
 
+
+
+
 #+ setup, include = FALSE
 knitr::opts_chunk$set(echo = TRUE, eval = TRUE, comment = NA, message = F, warning = F)
+# YAML Header and knitr chunk options aren't explicitly necessary but 
+# make everything look a little neater.
 
-#' ## Baseball Data
+#' We change between Rmarkdown, knitr and normal comments by using different types
+#' of hashtags. 
+#' 
+#' - `#` Everything after this hashtag is a normal R comment
+#' - `#'` Everything after this is Rmarkdown
+#' - `#+` Everything after this is knitr options
+#' 
+#' 
+#' 
+#' ## A Heading
+#' 
+#' 
+#' Some more text explaining what we are going to do...
+#' 
+#' ### Some R
 
-# load packages and data
-library(Lahman) # Contains baseball data
+#+ cars
+# Some R
+# loading packages
 library(dplyr)
-library(ggplot2)
-
-# Calculate batting statistics
-Batting = tbl_df(battingStats()) 
-Teams = tbl_df(Teams)
-Teams = select(Teams, yearID, teamID, franchID)
-Batting = left_join(Batting, Teams)
-my_fav_team = "BOS"
-team_name = TeamsFranchises$franchName[TeamsFranchises$franchID == my_fav_team]
-num_hr = 400 # Number of home runs to examine later
-
-
-# Select my team
-my_team = Batting %>%
-  filter(franchID == my_fav_team)
-
-# calculate average BA for the team that year
-ba_year = my_team %>%
-  group_by(yearID) %>%
-  summarise(average_ba = mean(BA, na.rm = T)) %>%
-  ungroup()
-
-#' Plot the batting average of the `r team_name`
-
-# plot the results
-ggplot(data = ba_year, mapping = aes(x = yearID, y = average_ba)) + 
-  geom_point() + 
-  xlab("Year") + ylab("Mean Batting Average") + 
-  ggtitle(paste("Mean Batting Average of the", team_name, sep = " "))
-
-# Examine the number of my players who have hit more than `num_hr` home runs
-
-# Gather the players 
-home_runs = my_team %>%
-  group_by(playerID) %>%
-  summarise(total_hr = sum(HR)) %>%
-  filter(total_hr >= num_hr) 
-
-# collect actual player names
-players = tbl_df(Master) %>% 
-  select(playerID, nameFirst, nameLast)
-
-# join dataframes together and output
-home_runs = left_join(home_runs, players) %>%
-  mutate(Player = paste(nameFirst, nameLast, sep = " ")) %>%
-  select(Player, total_hr) %>% # reordering columns
-  arrange(desc(total_hr))
-
-#' The number of players hitting more than `r num_hr` for `r team_name`.
-
-home_runs
+# Calculating average mpg of cars by number of cylinders in engine
+mtcars %>% 
+  group_by(cyl) %>%
+  select(mpg, hp) %>%
+  summarise_each(funs(mean))
 
